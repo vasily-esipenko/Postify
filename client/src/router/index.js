0d1b029f41b549bd4f7ca5a7429705cb47610a52
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-02 23:30:28 +0300

    Add src/router

---
+++ client/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Saved from '../views/Saved.vue';
+import Profile from '../views/Profile.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/saved',
+    name: 'Saved',
+    component: Saved
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
+  }
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
