ba02f604856802f0d99c9a5e2d2c33c4812e5cdf
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-02 23:30:59 +0300

    Add src/views

---
+++ client/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <h3>The latest posts:</h3>
+    <Post />
+</div>
+</template>
+
+<script>
+import Post from '@/components/Post.vue';
+
+export default {
+  name: 'Home',
+  components: {
+      Post,
+  },
+};
+</script>
---
+++ client/src/views/Saved.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h6>Development in progress...</h6>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Saved',
+}
+</script>
