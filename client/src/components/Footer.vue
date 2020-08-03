dbde6e4a1f2d433c9bf7a7e1510f58f3efad079d
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-03 20:59:43 +0300

    Update src/components/Footer.vue

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -2,7 +2,15 @@
     <footer class="footer-copyright">
             <div class="container">
             Vasily Esipenko, 2020
-            <a class="grey-text text-lighten-4 right" href="#!"></a>
+            <a class="about-a about-a--footer" href="https://t.me/Vasily_Esipenko" target="_blank">
+                <i class="fab fa-telegram"></i>
+            </a>
+            <a class="about-a about-a--footer" href="https://github.com/vasily-esipenko" target="_blank">
+                <i class="fab fa-github"></i>
+            </a>
+            <a class="about-a about-a--footer" href="https://stackoverflow.com/users/13187161/vasily-esipenko?tab=profile" target="_blank">
+                <i class="fab fa-stack-overflow"></i>
+            </a>
             </div>
     </footer>
 </template>
@@ -15,11 +23,25 @@ export default {
 
 <style scoped>
 .footer-copyright {
-    margin-top: auto;
-    bottom: 0;
-    left: 0;
     text-align: left;
+    height: 60px;
+    padding: 10px 0 0;
     position: relative;
-    display: none;
+    left: -15%;
+    margin: 10px 0 0;
+    font-size: 16px;
+}
+
+.about-a--footer {
+    display: inline-flex;
+    text-decoration: none;
+    color: #fafafa;
+    font-size: 16px;
+    margin-left: 12px;
+    transition: opacity .1s linear;
+}
+
+.about-a--footer:hover {
+    opacity: .8;
 }
 </style>
