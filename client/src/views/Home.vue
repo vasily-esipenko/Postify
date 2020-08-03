e61aa0e12a73d3e9d9edc38fab822cb88b07be14
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-03 15:25:35 +0300

    Update Home.vue

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,22 @@
 <template>
 <div>
-    <h3>The latest posts:</h3>
+    <PostForm />
+    <h4>The latest posts: {{ getMessages.length }}</h4>
     <Post />
 </div>
 </template>
 
 <script>
 import Post from '@/components/Post.vue';
+import PostForm from '@/components/PostForm.vue';
+import {mapGetters} from 'vuex';
 
 export default {
   name: 'Home',
   components: {
       Post,
+      PostForm
   },
+  computed: mapGetters(['getMessages'])
 };
 </script>
