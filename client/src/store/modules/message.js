716d38850980c44e0957fae50e53ecce032789a6
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-03 15:25:09 +0300

    Update message.js

--- client/src/store/modules/message.js
+++ client/src/store/modules/message.js
@@ -4,11 +4,26 @@ export default {
             const res = await fetch('http://localhost:7000/messages');
             const messages = await res.json();
             ctx.commit('updateMessages', messages);
+        },
+        async addMessage(ctx, message) {
+            const res = await fetch('http://localhost:7000/messages', {
+                method: "POST",
+                body: JSON.stringify(message),
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                }
+            });
+            const newMessage = await res.json();
+            ctx.commit('insertNewMessage', newMessage);
         }
     },
     mutations: {
         updateMessages(state, messages) {
             state.messages = messages;
+        },
+        insertNewMessage(state, message) {
+            state.messages.unshift(message);
         }
     },
     state: {
