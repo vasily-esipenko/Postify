d49fd297da42ce67b3fc14e6f18348865c1ac5e9
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-04 17:25:29 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,26 +4,31 @@ const config = require('config');
 const cors = require('cors');
 const morgan = require('morgan');
 
+// Add messages collection from the database
 const messages = require('./db/Message');
 
+// App initialization
 const app = express();
 
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use(cors());
 
+// Check server work
 app.get('/', (req, res) => {
     res.json({
         message: 'Server is working!'
     });
 });
 
+// Get all messages
 app.get('/messages', (req, res) => {
     messages.getAll().then(messages => {
         res.json(messages);
     });
 });
 
+// Post message to the messages' collection
 app.post('/messages', (req, res) => {
     console.log(req.body);
     messages.insertMsg(req.body).then(message => {
@@ -34,8 +39,10 @@ app.post('/messages', (req, res) => {
     });
 });
 
+// Port connection
 const PORT = config.get('port') || 7000;
 
+// Running app
 app.listen(PORT, () => {
     console.log(`Server is on post ${PORT}`);
 });
