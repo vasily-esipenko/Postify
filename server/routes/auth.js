21a200ab8c92e53f9e42bb9504bcbe98575b379a
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-05 18:04:39 +0300

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,23 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const users = require('../db/User');
 
-router.post('/register', (req, res) => {
-    res.send('Register');
+router.get('/', (req, res) => {
+    users.getAllUsers().then(users => {
+        res.json(users);
+    });
 });
 
-router.post('/login', (req, res) =>{
+router.post('/register', (req, res) => {
+    users.insertUser(req.body).then(user => {
+        res.json(user);
+    }).catch(error => {
+        res.status(500);
+        res.json(error);
+    });
+});
+
+router.get('/login', (req, res) =>{
     res.send('Login');
 });
 
