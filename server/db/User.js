0990a9ecf6387149b89071b51ec966fe7f72e47e
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-05 18:02:38 +0300

    Update User.js

--- server/db/User.js
+++ server/db/User.js
@@ -1,11 +1,12 @@
 const db = require('./connection');
 const Joi = require('joi');
+const bcrypt = require('bcrypt');
 
 // Validation schema
 const schema = Joi.object({
     username: Joi.string().max(30).alphanum().required(),
     email: Joi.string().email().required(),
-    password: Joi.string().min(8).required()
+    password: Joi.string().regex(/^[a-zA-Z0-9]{8,30}$/).required()
 });
 
 const users = db.get('users');
@@ -18,6 +19,9 @@ function insertUser(user) {
     const result = schema.validate(user);
     if (result.error == null) {
         user.created = new Date();
+        bcrypt.hash(user.password, 11).then(hashed => {
+            user.password = hashed;
+        });
         return users.insert(user);
     } else {
         result ;Promise.reject(result.error);
@@ -26,5 +30,5 @@ function insertUser(user) {
 
 module.exports = {
     getAllUsers,
-    insertUser
+    insertUser,
 }
