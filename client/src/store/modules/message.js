d608f3cd8d39faaecedc222975ccc5aecaec1d4a
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-05 18:02:16 +0300

    store/modules/message.js

--- client/src/store/modules/message.js
+++ client/src/store/modules/message.js
@@ -1,12 +1,12 @@
 export default {
     actions: {
         async fetchMessages(ctx) {
-            const res = await fetch('http://localhost:7000/messages');
+            const res = await fetch('http://localhost:7000/api/messages/get');
             const messages = await res.json();
             ctx.commit('updateMessages', messages);
         },
         async addMessage(ctx, message) {
-            const res = await fetch('http://localhost:7000/messages', {
+            const res = await fetch('http://localhost:7000/api/messages/insert', {
                 method: "POST",
                 body: JSON.stringify(message),
                 headers: {
