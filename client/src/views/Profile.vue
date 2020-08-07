628e3d0661e27e105db836ad7e4380775457ae3f
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-07 11:30:37 +0300

    Update Profile.vue

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
-        <h6>Development in progress...</h6>
+        <AuthForm />
     </div>
 </template>
 
 <script>
+import AuthForm from '../components/AuthForm.vue';
+
 export default {
     name: 'Profile',
+    components: {
+        AuthForm,
+    }
 }
 </script>
