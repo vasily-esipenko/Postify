6123286d8e982f757d76470245077d065fc1f14d
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-08 22:10:41 +0300

    Update auth.js

--- client/src/store/modules/auth.js
+++ client/src/store/modules/auth.js
@@ -9,11 +9,40 @@ export default {
                     "Accept": "application/json"
                 }
             });
+            const response = await res.json();
+            ctx.commit('setResult', response);
+        },
+        async loginUser(ctx, user) {
+            const res = await fetch('http://localhost:7000/api/user/login', {
+                method: 'POST',
+                body: JSON.stringify(user),
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                }
+            });
+            const response = await res.json();
+            ctx.commit('setLogResult', response);
         }
     },
-    mutations: {},
-    state: {
-        usernames: []
+    mutations: {
+        setResult(state, res) {
+            state.regResult = res;
+        },
+        setLogResult(state, res) {
+            state.logResult = res;
+        }
     },
-    getters: {}
+    state: {
+        regResult: "",
+        logResult: "",
+    },
+    getters: {
+        getResult(state) {
+            return state.regResult;
+        },
+        getLogResult(state) {
+            return state.logResult;
+        }
+    }
 }
