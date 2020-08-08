7e588ea50635a112a443496d76ba6b19e78a8d39
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-08 22:11:43 +0300

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -17,8 +17,8 @@ router.post('/register', (req, res, next) => {
             username: req.body.username,
         }).then(user => {
             if (user) {
-                const error = new Error('User already exists');
-                next(error);
+                const error = new Error('User with this username already exists');
+                res.json(error.message);
             } else {
                 bcrypt.hash(req.body.password, 11).then(hashed => {
                     const newUser = {
@@ -28,10 +28,10 @@ router.post('/register', (req, res, next) => {
                         created: new Date()
                     }
                     users.users.insert(newUser).then(insertedUser => {
-                        res.json(insertedUser).catch(error => {
+                        res.json("You have successfully signed up!");
+                        }).catch(error => {
                             res.status(500);
-                            res.json(error);
-                        });
+                            res.json(`Something went wrong: ${error}`);
                     });
                 });
             }
@@ -42,19 +42,37 @@ router.post('/register', (req, res, next) => {
 });
 
 router.post('/login', (req, res, next) =>{
-    const result = users.schema.validate(req.body);
+    const result = users.loginSchema.validate(req.body);
     if (result.error == null) {
         users.users.findOne({
-            username: req.body.username,
             email: req.body.email
         }).then(user => {
             if (user) {
                 bcrypt.compare(req.body.password, user.password).then(result => {
-                    res.json(result);
+                    if (result) {
+                        const payload = {
+                            _id: user._id,
+                            username: user.username
+                        };
+                        jwt.sign(payload, config.tokenSecret, {
+                            expiresIn: '1d'
+                        }, (err, token) => {
+                            if (err) {
+                                res.status(422);
+                                res.json(err);
+                            } else {
+                                const genToken = token;
+                                res.json('You have successfully logged in!');
+                            }
+                        });
+                    } else {
+                        res.status(422);
+                        res.json('Wrong password');
+                    }
                 });
             } else {
                 const error = new Error('There is no such user');
-                next(error);
+                res.json(error.message);
             }
         })
     } else {
