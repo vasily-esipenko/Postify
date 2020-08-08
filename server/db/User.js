f908a14689f01eb6c9f2030b60a99b0fd4fab97a
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-08 22:11:24 +0300

    Update User.js

--- server/db/User.js
+++ server/db/User.js
@@ -1,15 +1,20 @@
 const db = require('./connection');
 const Joi = require('joi');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
-// Validation schema
+// Sign up validation schema
 const schema = Joi.object({
     username: Joi.string().max(30).alphanum().required(),
     email: Joi.string().email().required(),
     password: Joi.string().regex(/^[a-zA-Z0-9]{8,30}$/).required()
 });
 
+// Log in validation schema
+const loginSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().regex(/^[a-zA-Z0-9]{8,30}$/).required()
+});
+
+// Db connection
 const users = db.get('users');
 
 function getAllUsers() {
@@ -19,5 +24,6 @@ function getAllUsers() {
 module.exports = {
     getAllUsers,
     schema,
+    loginSchema,
     users,
 }
