b58bd2fe5771f3c6506c3d4af22b8865ab20d8f0
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-09 19:47:04 +0300

    Update Like.vue

--- client/src/components/buttons/Like.vue
+++ client/src/components/buttons/Like.vue
@@ -1,12 +1,12 @@
 <template>
     <span class="like" @click="toggleLike">
         <span v-if="liked">
-            <span class="liked">
-            <i class="fas fa-heart"></i>
+            <span class="liked" @click="removeLike">
+            <i class="fas fa-heart"></i> {{ numberOfLikes }}
             </span>
         </span>
         <span v-else>
-            <span class="notliked">
+            <span class="notliked" @click="addLike">
                 <i class="far fa-heart"></i>
             </span>
         </span>
@@ -18,12 +18,19 @@ export default {
     name: 'Like',
     data() {
         return {
-            liked: false
+            liked: false,
+            numberOfLikes: 0
         }
     },
     methods: {
         toggleLike() {
             this.liked = !this.liked;
+        },
+        addLike() {
+            this.numberOfLikes++;
+        },
+        removeLike() {
+            this.numberOfLikes--;
         }
     }
 }
