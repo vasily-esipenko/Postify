037736e3c86bec1b5b01326000bfc603195c8952
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-10 18:29:58 +0300

    Update Saved.vue

--- client/src/views/Saved.vue
+++ client/src/views/Saved.vue
@@ -1,11 +1,30 @@
 <template>
     <div>
-        <h6>Development in progress...</h6>
+        <div v-if="isLoggedIn">
+            <h4>Your saved posts</h4>
+        </div>
+        <div v-else>
+            <AuthForm />
+        </div>
     </div>
 </template>
 
 <script>
+import AuthForm from '../components/auth/AuthForm.vue';
+
 export default {
     name: 'Saved',
+    components: {
+        AuthForm,
+    },
+    computed: {
+        isLoggedIn() {
+            if (localStorage.getItem("token") && localStorage.getItem("token") != "undefined") {
+                return true;
+            }
+
+            return false;
+        }
+    }
 }
 </script>
