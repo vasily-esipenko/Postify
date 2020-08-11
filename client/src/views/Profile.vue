0bce99dee4aa19492774c4ad32e5b9ecbbb8f681
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-11 13:07:59 +0300

    Update Profile.vue

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -2,6 +2,9 @@
     <div>
         <div v-if="isLoggedIn">
             <h4>This is your profile!</h4>
+            <div class="profile">
+                {{ getUserData }}
+            </div>
         </div>
         <div v-else>
             <AuthForm />
@@ -11,19 +14,43 @@
 
 <script>
 import AuthForm from '../components/auth/AuthForm.vue';
+import {mapGetters, mapActions} from 'vuex';
+import jwt from 'jsonwebtoken';
+import config from '/Users/esipe/Postify/server/config/default.json';
 
 export default {
     name: 'Profile',
     components: {
         AuthForm,
     },
+    data() {
+        return {
+            isLogged: false,
+        }
+    },
+    methods: {
+        ...mapActions(['addUserData']),
+    },
     computed: {
+        ...mapGetters(['getUserData']),
         isLoggedIn() {
             if (localStorage.getItem("token") && localStorage.getItem("token") != "undefined") {
-                return true;
+                jwt.verify(JSON.parse(localStorage.getItem("token")), config.tokenSecret, (err, decoded) => {
+                    if (err) {
+                        console.log(err.message);
+                        return false;
+                    }
+                    
+                    if (decoded) {
+                        console.log(decoded);
+                        const decodedData = decoded;
+                        this.addUserData(decodedData);
+                        this.isLogged = true;
+                    }  
+                });
             }
 
-            return false;
+            return this.isLogged;
         }
     }
 }
