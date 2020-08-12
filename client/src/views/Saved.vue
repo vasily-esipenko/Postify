97fe57948d43892aa6e108f0f5d8a770f51bed0c
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-12 22:12:38 +0300

    Update Saved.vue

--- client/src/views/Saved.vue
+++ client/src/views/Saved.vue
@@ -9,8 +9,27 @@
 
         <hr>
 
-        <div v-if="savedPosts.length">
-            {{ savedPosts }}
+        <div v-if="getSavedPosts.length">
+            <div class="post" v-for="post in getSavedPosts" v-bind:key="post._id">
+            <div class="post-header">
+                <span>
+                <img class="userpic" alt="userpic" src="https://via.placeholder.com/40">
+                <span class="username">{{ post.username }}</span>
+                </span>
+            </div>
+            <div class="post-body">
+                <div class="post-subject">{{ post.subject }}</div>
+                <p class="post-text">{{ post.message }}</p>
+            </div>
+            <div class="reaction-bar">
+                <span> 
+                    <Like /> 
+                    <Share />
+                    <Save/>
+                    <span class="date"> {{ new Date(post.created).toLocaleDateString() }} </span>
+                </span>
+            </div>
+        </div>
         </div>
         <div v-else>
             <p class="noposts">You have no saved posts right now</p>
@@ -19,8 +38,11 @@
 </template>
 
 <script>
+import Like from '../components/buttons/Like.vue';
+import Save from '../components/buttons/Save.vue';
+import Share from '../components/buttons/Share.vue';
 import AuthForm from '../components/auth/AuthForm.vue';
-import {mapActions} from 'vuex';
+import {mapActions, mapGetters} from 'vuex';
 import jwt from 'jsonwebtoken';
 import config from '/Users/esipe/Postify/server/config/default.json';
 
@@ -28,10 +50,12 @@ export default {
     name: 'Saved',
     components: {
         AuthForm,
+        Like,
+        Save,
+        Share,
     },
     data() {
         return {
-            savedPosts: [],
             isLogged: false,
         }
     },
@@ -39,6 +63,7 @@ export default {
         ...mapActions(['addUserData']),
     },
     computed: {
+        ...mapGetters(['getSavedPosts']),
         isLoggedIn() {
             if (localStorage.getItem("token") && localStorage.getItem("token") != "undefined") {
                 jwt.verify(JSON.parse(localStorage.getItem("token")), config.tokenSecret, (err, decoded) => {
@@ -48,7 +73,6 @@ export default {
                     }
                     
                     if (decoded) {
-                        console.log(decoded);
                         const decodedData = decoded;
                         this.addUserData(decodedData);
                         this.isLogged = true;
@@ -61,3 +85,62 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+
+.post {
+    width: 60%;
+    height: auto;
+    margin: 40px auto;
+    padding: 1rem;
+    border: 1px solid #777;
+    border-radius: 3px;
+    z-index: 1;
+}
+
+.post-header {
+    text-align: left;
+    font-size: 14px;
+    margin-bottom: 4px;
+    width: 30%;
+    border: 1px solid #777;
+    border-radius: 3px;
+    padding: 4px;
+}
+
+.username {
+    display: inline;
+    margin-left: 1rem;
+    font-weight: 600;
+    font-style: italic;
+}
+
+.post-body {
+    text-align: left;
+    border-bottom: 1px solid #777;
+}
+
+.post-subject {
+    text-align: center;
+    font-size: 20px;
+}
+
+.post-text {
+    margin-top: 4px;
+    font-size: 17px;
+    overflow-wrap: break-word;
+}
+
+.reaction-bar {
+    margin: 1rem 1.5rem 0;
+    padding: 0;
+    text-align: left;
+}
+
+.date {
+    font-size: 15px;
+    margin-left: 30rem;
+    text-align: right;
+}
+
+</style>
