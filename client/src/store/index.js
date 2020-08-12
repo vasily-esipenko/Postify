6e9dfeb063522cc1566f6e218c87662b9f1fba26
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-12 22:11:37 +0300

    Update index.js

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import message from './modules/message';
 import auth from './modules/auth';
+import saved from './modules/saved';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
       message,
-      auth
+      auth,
+      saved
   },
 });
