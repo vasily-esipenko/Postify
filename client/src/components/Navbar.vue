e3ab9f90c9b289f7b66efa8258e96704e69f50cd
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-13 18:38:06 +0300

    Update Navbar.vue

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -3,7 +3,7 @@
         <nav>
             <div class="nav-wrapper grey darken-3">
             <router-link to="/" class="brand-logo">Postify</router-link>
-            <ul id="nav-mobile" class="right hide-on-med-and-down right">
+            <ul class="right hide-on-med-and-down right">
                 <li><router-link to="/saved" class="blue-text text-darken-1">Saved</router-link></li>
                 <li><router-link to="/profile" class="blue-text text-darken-1">Profile</router-link></li>
                 <li><a class="blue-text text-darken-1" @click="logout" v-show="isLoggedIn">Logout</a></li>
@@ -26,6 +26,7 @@ export default {
             location.reload();
         }
     },
+
     computed: {
         isLoggedIn() {
             if (localStorage.getItem("token") && localStorage.getItem("token") != "undefined") {
