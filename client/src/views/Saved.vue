d7aed0e23ddf72ea36b7d67aaf8579d4f867d662
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-13 22:53:58 +0300

    Update Saved.vue

--- client/src/views/Saved.vue
+++ client/src/views/Saved.vue
@@ -2,39 +2,38 @@
     <div>
         <div v-if="isLoggedIn">
             <h5>Your saved posts</h5>
+            <hr>
+            <div v-if="getSavedPosts.length">
+                <div class="post" v-for="post in getSavedPosts" v-bind:key="post._id">
+                    <div class="post-header">
+                        <span>
+                        <img class="userpic" alt="userpic" src="https://via.placeholder.com/40">
+                        <span class="username">{{ post.username }}</span>
+                        </span>
+                    </div>
+                    <div class="post-body">
+                        <div class="post-subject">{{ post.subject }}</div>
+                        <p class="post-text">{{ post.message }}</p>
+                    </div>
+                    <div class="reaction-bar">
+                        <span> 
+                        <span class="buttons">
+                            <i class="far fa-heart"></i>
+                            <i class="fas fa-share" id="share"></i>
+                            <i class="fas fa-bookmark" id="save"></i>
+                            <i class="fas fa-ban" id="remove" @click="removeSavedPost(post._id)"></i>
+                        </span>
+                        <span class="date"> {{ new Date(post.created).toLocaleDateString() }} </span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div v-else>
+            <p class="noposts">You have no saved posts right now</p>
+            </div>
         </div>
         <div v-else>
             <AuthForm />
-        </div>
-
-        <hr>
-
-        <div v-if="getSavedPosts.length">
-            <div class="post" v-for="post in getSavedPosts" v-bind:key="post._id">
-            <div class="post-header">
-                <span>
-                <img class="userpic" alt="userpic" src="https://via.placeholder.com/40">
-                <span class="username">{{ post.username }}</span>
-                </span>
-            </div>
-            <div class="post-body">
-                <div class="post-subject">{{ post.subject }}</div>
-                <p class="post-text">{{ post.message }}</p>
-            </div>
-            <div class="reaction-bar">
-                <span> 
-                    <span class="buttons">
-                        <i class="far fa-heart"></i>
-                        <i class="fas fa-share" id="share"></i>
-                        <i class="fas fa-bookmark" id="save"></i>
-                    </span>
-                    <span class="date"> {{ new Date(post.created).toLocaleDateString() }} </span>
-                </span>
-            </div>
-        </div>
-        </div>
-        <div v-else>
-            <p class="noposts">You have no saved posts right now</p>
         </div>
     </div>
 </template>
@@ -57,6 +56,22 @@ export default {
     },
     methods: {
         ...mapActions(['addUserData']),
+        removeSavedPost(id) {
+            const foundPostIndex = this.getSavedPosts.indexOf(this.findSavedPost(id));
+            if (foundPostIndex >= 0) {
+                this.getSavedPosts.splice(foundPostIndex, 1);
+                localStorage.setItem("savedPosts", JSON.stringify(this.getSavedPosts));
+                location.reload();
+            }
+
+        },
+        findSavedPost(id) {
+            for (let i = 0; i < this.getSavedPosts.length; i++) {
+                if (this.getSavedPosts[i]._id == id) {
+                    return this.getSavedPosts[i];
+                }
+            }
+        }
     },
     computed: {
         ...mapGetters(['getSavedPosts']),
@@ -97,6 +112,18 @@ export default {
 #save {
     margin-left: 2rem;
     color: #1D9AF2;
+}
+
+#remove {
+    margin-left: 2rem;
+    cursor: pointer;
+    color: #fafafa;
+    transition: all .1s linear;
+}
+
+#remove:hover {
+    color: #f22;
+    font-size: 18.2px;
 }
 
 .date {
