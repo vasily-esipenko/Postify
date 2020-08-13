c05cea84c5099cd791e7435ead383fa8163c3afb
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-13 22:53:40 +0300

    Update saved.js

--- client/src/store/modules/saved.js
+++ client/src/store/modules/saved.js
@@ -1,9 +1,19 @@
-import auth from './auth';
-
 export default {
     actions: {
         async addToSavedPosts(ctx, posts) {
             ctx.commit('setSavedPost', posts);
+        },
+        async addSavedPostsToServer(ctx, userEmail, savedPosts) {
+            const res = await fetch('http://localhost:7000', {
+                method: "POST",
+                body: {email: userEmail, savedPosts: savedPosts},
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                }
+            });
+            const response = await res.json()
+            console.log(response);
         }
     },
     mutations: {
