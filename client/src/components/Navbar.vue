b618fc1fdead33444fc34384917080bff9c07819
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-15 13:10:09 +0300

    Update Navbar.vue

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,15 +1,32 @@
 <template>
-    <div class="navbar-fixed">
-        <nav>
-            <div class="nav-wrapper grey darken-3">
-            <router-link to="/" class="brand-logo">Postify</router-link>
-            <ul class="right hide-on-med-and-down right">
-                <li><router-link to="/saved" class="blue-text text-darken-1">Saved</router-link></li>
-                <li><router-link to="/profile" class="blue-text text-darken-1">Profile</router-link></li>
-                <li><a class="blue-text text-darken-1" @click="logout" v-show="isLoggedIn">Logout</a></li>
-            </ul>
-            </div>
-        </nav>
+    <div>
+        <div class="navbar-fixed">
+            <nav>
+                <div class="nav-wrapper grey darken-3">
+                <router-link to="/" class="brand-logo">Postify</router-link>
+                <ul class="right hide-on-med-and-down right">
+                    <li><router-link to="/saved" class="blue-text text-darken-1">Saved</router-link></li>
+                    <li><router-link to="/profile" class="blue-text text-darken-1">Profile</router-link></li>
+                    <li><a class="blue-text text-darken-1" @click="logout" v-show="isLoggedIn">Logout</a></li>
+                </ul>
+                </div>
+            </nav>
+        </div>
+        <div class="navbar-fixed-mobile">
+            <nav>
+                <div class="nav-wrapper nav-wrapper-mobile grey darken-3">
+                    <router-link to="/" class="brand-logo">Postify</router-link>
+                    <span class="burger-menu-button" @click="displayMenu"><i class="fas fa-bars"></i></span>
+                    <div class="burger-menu" v-show="displayBurgerMenu">
+                        <ul>
+                            <li><router-link to="/saved" class="blue-text text-darken-1">Saved</router-link></li>
+                            <li><router-link to="/profile" class="blue-text text-darken-1">Profile</router-link></li>
+                            <li><a class="blue-text text-darken-1" @click="logout" v-show="isLoggedIn">Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
     </div>
 </template>
 
@@ -18,6 +35,11 @@ import {mapActions, mapGetters} from 'vuex';
 
 export default {
     name: 'Navbar',
+    data() {
+        return {
+            displayBurgerMenu: false,
+        };
+    },
     methods: {
         ...mapActions(['removeUserData', 'addSavedPostsToServer']),
         logout() {
@@ -30,6 +52,9 @@ export default {
             this.addSavedPostsToServer(savedData);
             localStorage.removeItem("savedPosts");
             location.reload();
+        },
+        displayMenu() {
+            this.displayBurgerMenu = !this.displayBurgerMenu;
         }
     },
 
@@ -41,7 +66,7 @@ export default {
             }
 
             return false;
-        }
+        },
     }
 };
 </script>
@@ -55,4 +80,31 @@ nav {
     padding: 0 2rem;
 }
 
+.navbar-fixed-mobile {
+    display: none;
+}
+
+.burger-menu-button {
+    cursor: pointer;
+}
+
+.burger-menu {
+    border: 1px solid #333;
+    border-radius: 3px;
+    width: 35%;
+    z-index: 1;
+    background-color: #333;
+    position: absolute;
+    padding: .8rem;
+}
+
+@media (max-width: 993px) {
+    .navbar-fixed-mobile {
+        display: block;
+    }
+
+    .navbar-fixed {
+        display: none;
+    }
+}
 </style>
