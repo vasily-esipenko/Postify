b7f9cc8ccbc086ac396c80df6f67edf210033826
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-16 17:24:47 +0300

    Update Signup.vue

--- client/src/components/auth/Signup.vue
+++ client/src/components/auth/Signup.vue
@@ -29,8 +29,8 @@ const schema = Joi.object({
   username: Joi.string().regex(/(^[a-zA-Z0-9_]+$)/).min(2).max(30)
     .required(),
   email: Joi.string().email({ tlds: {allow: false} }).required(),
-  password: Joi.string().trim().min(8).required(),
-  confirmPassword: Joi.string().trim().min(8).required(),
+  password: Joi.string().regex(/^[a-zA-Z0-9]{8,30}$/).min(8).required(),
+  confirmPassword: Joi.string().regex(/^[a-zA-Z0-9]{8,30}$/).min(8).required(),
 });
 
 export default {
@@ -55,7 +55,7 @@ export default {
                     username: this.authForm.username,
                     email: this.authForm.email,
                     password: this.authForm.password
-                }
+                };
                 this.registerUser(user);
                 this.authForm.username = this.authForm.email = this.authForm.password = this.authForm.confirmPassword = "";
                 setTimeout(() => {
