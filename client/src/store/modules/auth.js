936b48cbe6acbb24514a5b44e22a203a1c95ab4f
vasily-esipenko <esipenkovasilij@gmail.com>
2020-08-17 11:25:26 +0300

    Update auth.js

--- client/src/store/modules/auth.js
+++ client/src/store/modules/auth.js
@@ -3,6 +3,7 @@ export default {
         async registerUser(ctx, user) {
             const res = await fetch('http://localhost:7000/api/user/register', {
                 method: 'POST',
+                mode: 'no-cors',
                 body: JSON.stringify(user),
                 headers: {
                     "Content-Type": "application/json",
@@ -22,6 +23,7 @@ export default {
         async loginUser(ctx, user) {
             const res = await fetch('http://localhost:7000/api/user/login', {
                 method: 'POST',
+                mode: 'no-cors',
                 body: JSON.stringify(user),
                 headers: {
                     "Content-Type": "application/json",
@@ -45,6 +47,7 @@ export default {
         async verifyUser(ctx, token) {
             const res = await fetch('http://localhost:7000/api/user/verify', {
                 method: "POST",
+                mode: 'no-cors',
                 body: JSON.stringify(token),
                 headers: {
                     "Content-Type": "application/json",
